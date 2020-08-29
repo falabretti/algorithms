11d7163d02bff711a20962fa6bc8f5413fbe8c92
Lucas Falabretti <lucasfalabretti7@gmail.com>
2020-08-29 15:22:03 -0300

    Update dfs

--- graphs/dfs.cpp
+++ graphs/dfs.cpp
@@ -1,33 +1,65 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-bool visited[102];
-vector<int> adj[102];
+#define MAX 101
 
-void dfs(int v) {
-    visited[v] = true;
-    for (int a : adj[v]) {
-        if (visited[a]) continue;
-        dfs(a);
+bool visited[MAX];
+vector<int> adj[MAX];
+
+void add_edge(int v, int u) {
+
+    adj[v].push_back(u);
+    adj[u].push_back(v);
+}
+
+void dfs(int cur) {
+
+    if (visited[cur]) {
+        return;
+    }
+
+    visited[cur] = true;
+    cout << cur << " ";
+
+    for (int next : adj[cur]) {
+        dfs(next);
     }
 }
 
-int main() {
-    memset(visited, 0, sizeof(visited));
-    for (int i = 0; i < 102; i++) adj[i].clear();
+int count(int node_size) {
 
-    int n;
-    cin >> n;
+    int c = 0;
 
-    for (int i = 0; i < n - 1; i++) {
-        int a, b;
-        cin >> a >> b;
-
-        adj[a].push_back(b);
-        adj[b].push_back(a);
+    for (int cur = 0; cur < node_size; cur++) {        
+        if (!visited[cur]) {
+            c++;
+            dfs(cur);
+        }
     }
 
-    // dfs(v);
+    return c;
+}
+
+int main() {
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    memset(visited, 0, sizeof(visited));
+    for (auto& vec : adj) vec.clear();
+
+    int n = 4;
+    int e = 6;
+
+    add_edge(0, 1);
+    add_edge(0, 2);
+    add_edge(1, 2);
+    add_edge(2, 0);
+    add_edge(2, 3);
+    add_edge(3, 3);
+
+    dfs(2);
+    cout << "\n";
 
     return 0;
 }
